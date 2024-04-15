bf43b28b5c09c775629995ea9b81c1656579b5de
Simon Nickel <simonnickel@googlemail.com>
2024-04-15 12:43:09 +0200

    Update package

--- Package.swift
+++ Package.swift
@@ -5,19 +5,41 @@ import PackageDescription
 
 let package = Package(
     name: "snap-template-shared",
+	platforms: [
+		.iOS(.v17), .macOS(.v14)
+	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SnapTemplateShared",
-            targets: ["SnapTemplateShared"]),
+            targets: ["SnapTemplateShared"]
+		),
     ],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		.package(url: "https://github.com/simonnickel/snap-core.git", branch: "main"),
+		.package(url: "https://github.com/simonnickel/snap-theme.git", branch: "main"),
+		.package(url: "https://github.com/simonnickel/snap-settings-service.git", branch: "main"),
+		.package(url: "https://github.com/simonnickel/snap-matching-navigation.git", branch: "main"),
+	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SnapTemplateShared"),
+		// Targets can depend on other targets in this package and products from dependencies.
+		.target(
+			name: "SnapTemplateShared",
+			dependencies: [
+				.product(name: "SnapCore", package: "snap-core"),
+				.product(name: "SnapTheme", package: "snap-theme"),
+				.product(name: "SnapSettingsService", package: "snap-settings-service"),
+				.product(name: "SnapMatchingNavigation", package: "snap-matching-navigation"),
+			],
+			swiftSettings: [
+				.enableExperimentalFeature("StrictConcurrency")
+			]
+		),
         .testTarget(
             name: "SnapTemplateSharedTests",
-            dependencies: ["SnapTemplateShared"]),
+            dependencies: ["SnapTemplateShared"]
+		),
     ]
 )
