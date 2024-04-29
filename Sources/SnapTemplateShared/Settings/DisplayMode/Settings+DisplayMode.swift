7b669a36ebb77b15617cd58a074e4d43978b0109
Simon Nickel <simonnickel@googlemail.com>
2024-04-29 10:38:56 +0200

    Fix setting to be locally

--- Sources/SnapTemplateShared/Settings/DisplayMode/Settings+DisplayMode.swift
+++ Sources/SnapTemplateShared/Settings/DisplayMode/Settings+DisplayMode.swift
@@ -9,6 +9,6 @@ import SnapSettingsService
 
 public extension SettingsService.SettingDefinition {
 	
-	static let displayMode = SettingsService.Setting<DisplayMode>("Settings.Appearance.DisplayMode", in: .ubiquitous, default: .system)
+	static let displayMode = SettingsService.Setting<DisplayMode>("Settings.Appearance.DisplayMode", in: .defaults, default: .system)
 	
 }
