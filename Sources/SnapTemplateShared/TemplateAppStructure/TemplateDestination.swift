8896815ed6f687838e7ac725fee707080d785d03
Simon Nickel <simonnickel@googlemail.com>
2024-05-29 11:05:38 +0200

    Add TemplateDestination.swift

---
+++ Sources/SnapTemplateShared/TemplateAppStructure/TemplateDestination.swift
@@ -0,0 +1,33 @@
+//
+//  TemplateDestination.swift
+//  SnapTemplateShared
+//
+//  Created by Simon Nickel on 29.05.24.
+//
+
+import SwiftUI
+import SnapTheme
+import SnapMatchingNavigation
+
+public protocol TemplateDestination: MNavItem {
+	
+	var title: String? { get }
+	
+	var icon: Theme.IconKey? { get }
+	
+}
+
+public extension TemplateDestination {
+	
+	
+	// MARK: MNavItem
+	
+	var tabItem: any View {
+		ThemeTabItem(text: title, icon: icon)
+	}
+	
+	var idDefault: String {
+		String(reflecting: self).split(by: ["."]).last ?? ""
+	}
+	
+}
