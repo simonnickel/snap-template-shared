d2004d8605486be73c814d06d8eea1b3b68f4e9c
Simon Nickel <simonnickel@googlemail.com>
2024-10-26 14:12:23 +0200

    Update packages, fix settings button

--- Sources/SnapTemplateShared/TemplateAppStructure/TemplateContent.swift
+++ Sources/SnapTemplateShared/TemplateAppStructure/TemplateContent.swift
@@ -8,24 +8,25 @@ import SnapNavigation
 	
 public struct TemplateContent<NavigationProvider: SnapNavigationProvider>: View {
 	
-	private let provider: NavigationProvider
+	private let navigator: SnapNavigation.Navigator<NavigationProvider>
+	private let settingsScreen: NavigationProvider.Screen
 	
-	public init(provider: NavigationProvider) {
-		self.provider = provider
+	public init(provider: NavigationProvider, settingsScreen: NavigationProvider.Screen) {
+		self.navigator = SnapNavigation.Navigator(provider: provider)
+		self.settingsScreen = settingsScreen
 	}
 	
 	public var body: some View {
 		
 		SnapNavigationView(
-			provider: provider
+			navigator: navigator
 		)
 #if !os(macOS) // macOS settings are available in the application menu.
 		.tabViewSidebarBottomBar {
 			HStack {
-				// TODO: Needs to be inside of SnapNavigationView to have access to Navigator.
-//				ToolbarButtonSettings {
-//					navigator.present(screen: .settings)
-//				}
+				ToolbarButtonSettings {
+					navigator.present(screen: settingsScreen, style: .modal)
+				}
 				Spacer()
 			}
 		}
