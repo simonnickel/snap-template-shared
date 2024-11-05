b7e74e04eef8f2b387142e2156aa60b64627bbb7
Simon Nickel <simonnickel@googlemail.com>
2024-11-05 23:06:19 +0100

    Move settings button to template

--- Sources/SnapTemplate/TemplateContentSetupModifier.swift
+++ Sources/SnapTemplate/TemplateContentSetupModifier.swift
@@ -10,23 +10,36 @@ import SwiftUI
 
 public extension View {
 	
-	func setupTemplateConfig<Destination: SnapNavigationDestination>(for scene: SnapNavigation.NavigationScene<Destination>) -> some View {
-		modifier(TemplateContentSetupModifier<Destination>(scene: scene))
+	func setupTemplateConfig<Destination: SnapNavigationDestination, SidebarModifier: ViewModifier>(
+		for scene: SnapNavigation.NavigationScene<Destination>,
+		sidebarModifier: SidebarModifier
+	) -> some View {
+		modifier(TemplateContentSetupModifier<Destination, SidebarModifier>(scene: scene, sidebarModifier: sidebarModifier))
 	}
 	
 }
 
-internal struct TemplateContentSetupModifier<Destination: SnapNavigationDestination> : ViewModifier {
+internal struct TemplateContentSetupModifier<Destination: SnapNavigationDestination, SidebarModifier: ViewModifier> : ViewModifier {
 
 	@Dependency(\.templateState) private var templateState: TemplateState
 	
 	let scene: SnapNavigation.NavigationScene<Destination>
+	let sidebarModifier: SidebarModifier
 	
 	internal func body(content: Content) -> some View {
 		content
 			.navigationStyle(scene == .main ? .tabsAdaptable : nil) // TODO: Style from settings
 			.theme(apply: templateState.theme) // TODO: Check if updates are propagated.
 			.preferredColorScheme(templateState.displayMode?.colorScheme)
+#if !os(macOS) // macOS settings are available in the application menu.
+			.tabViewSidebarBottomBar {
+				HStack {
+					ToolbarButtonSettings()
+					Spacer()
+				}
+				.modifier(sidebarModifier)
+			}
+#endif
 	}
 	
 }
